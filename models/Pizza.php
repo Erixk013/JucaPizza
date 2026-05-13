f5d289728cc75e7c72c29e81e05488cffdb05ebc
Erixk013 <erick.oliveira.domingos587@gmail.com>
2026-05-13 19:20:12 -0300

    att8

--- models/Pizza.php
+++ models/Pizza.php
@@ -119,24 +119,3 @@ class Pizza
         return $stmt->execute();
     }
 }
-
-$query = 'INSERT INTO ' . $this->table_name . ' SET nome = :nome, ingredientes = :ingredientes, valor = :valor';
- 
-        // Preparar a query
-        $stmt = $this->conn->prepare($query);
- 
-        // Limpar os dados
-        $this->nome = htmlspecialchars(strip_tags($this->nome));
-        $this->ingredientes = htmlspecialchars(strip_tags($this->ingredientes));
-        $this->valor = htmlspecialchars(strip_tags($this->valor));
- 
-        // Vincular os parâmetros
-        $stmt->bindParam(':nome', $this->nome);
-        $stmt->bindParam(':ingredientes', $this->ingredientes);
-        $stmt->bindParam(':valor', $this->valor);
- 
-        // Executar a query
-        if ($stmt->execute()) {
-            return true;
-        }        
-        return false;
